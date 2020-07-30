432e45fb2d2c9107dea524a64a3239cd26a7edb3
Rafael Toledano <rafael.toledano@numbrs.com>
2020-07-30 20:20:54 +0200

    Updates

---
+++ precache-manifest.103cb6ec548a8d7ec7c0ff3ae59842f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4677c63168443c437196d48058680731",
+    "url": "/mock-risk-app/index.html"
+  },
+  {
+    "revision": "201911928f3d12af6a33",
+    "url": "/mock-risk-app/static/css/main.dfd7c2a8.chunk.css"
+  },
+  {
+    "revision": "070a98b982c3dbca9ca2",
+    "url": "/mock-risk-app/static/js/2.af50f3d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mock-risk-app/static/js/2.af50f3d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201911928f3d12af6a33",
+    "url": "/mock-risk-app/static/js/main.30a1ea0e.chunk.js"
+  },
+  {
+    "revision": "c99b8e56760c6aeed75d",
+    "url": "/mock-risk-app/static/js/runtime-main.115f7efd.js"
+  }
+]);
